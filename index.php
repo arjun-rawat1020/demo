001930b54b50ca1bbdbb85ee23905f043416f9e8
arjun-rawat1020 <52284607+arjun-rawat1020@users.noreply.github.com>
2019-06-27 18:13:24 +0530

    Uploaded index page

---
+++ index.php
@@ -0,0 +1,209 @@
+<?php include("head.php"); ?>
+<body class="home-page">
+<?php include("header.php"); ?>
+
+    <section class="banner">
+        <img src="img/banner.jpg" alt="Discover & Explore New Paths" class="wow fadeInDown">
+        <div class="wrapper">
+            <div class="banner-content">
+                <div class="banner-heading wow bounceInUp"  data-wow-delay="0s">Discover & Explore New Paths</div>
+                <p class="wow bounceInUp" data-wow-delay="0.5s">The first step always decides the journey</p>
+                <div class="readmore explore wow bounceInUp" data-wow-delay="1s"><a href="#about-us">Explore Now</a></div>
+            </div>
+        </div>
+    </section>
+    <section class="about-us" id="about-us">
+        <div class="wrapper">
+            <div class="left wow fadeInLeft">
+                <div class="image"><img src="img/aboutus.jpg" alt="About Us"></div>
+            </div>
+            <div class="right wow ">
+                <div class="heading wow fadeInDown"><span>Get To Know</span>About Us</div>
+                <p class="wow fadeInUp">DP Jain & Co. Infrastructure Pvt. Ltd. is part of DP Jain Group led by Mr. Deepak Jain and Mr. Girish Jain. The Group was originally founded by their father Late <strong>Mr. Dharampal Jain</strong>, in partnership with <strong>Mr. Nemichand Jain</strong> in 1975, in Nagpur, India.</p>
+                <p class="wow fadeInUp">Over the years, the company has steadily grown and now an emerging civil infrastructure projects construction and development company in India. It delivers projects, in both EPC and PPP mode, in several high growth sectors, such as, Roads & Highways, Bridges, Airports, Water Irrigation, Institutional Buildings, Railways, Power, Mining and Heavy Earth Work projects.</p>
+                <div class="readmore"><a href="javascript:void(0);">Read More</a></div>
+                <div class="statistics wow slideInRight">
+                    <div class="label">Our Statistics</div>
+                    <div class="statistics-list">
+                        <ul>
+                            <li class="wow slideInRight" data-wow-delay="0.5s">
+                                <div class="list-heading"><span>75</span>+</div>
+                                <p><span>Projects</span>Completed</p>
+                            </li>
+                            <li class="wow slideInRight" data-wow-delay="0.8s">
+                                <div class="list-heading"><span>100</span>%</div>
+                                <p><span>Client</span>Satisfactions</p>
+                            </li>
+                            <li class="wow slideInRight" data-wow-delay="1.1s">
+                                <div class="list-heading"><span>05</span></div>
+                                <p><span>Awards</span>Winner</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="service">
+        <div class="wrapper">
+            <img src="img/bg-service.jpg" alt="" class="wow slideInRight" data-wow-delay="0.5s">
+            <div class="heading wow fadeInDown"><span>Let us help you</span>Services</div>
+            <div class="service-list service-slider">
+                <ul>
+                    <li class="wow fadeInLeft" data-wow-delay="1s">
+                        <div class="image"><img src="img/products/roads-highways.jpg" alt=""></div>
+                        <div class="product-name">Roads & Highways</div>
+                        <div class="readmore"><a href="javascript:void(0);">Explore Now</a></div>
+                    </li>
+                    <li class="wow fadeInLeft" data-wow-delay="1.5s">
+                        <div class="image"><img src="img/products/railway-metro-rail.jpg" alt=""></div>
+                        <div class="product-name">Railway &amp; Metro Rail</div>
+                        <div class="readmore"><a href="javascript:void(0);">Explore Now</a></div>
+                    </li>
+                    <li class="wow fadeInRight" data-wow-delay="2s">
+                        <div class="image"><img src="img/products/airport.jpg" alt=""></div>
+                        <div class="product-name">Airport</div>
+                        <div class="readmore"><a href="javascript:void(0);">Explore Now</a></div>
+                    </li>
+                    <li class="wow fadeInRight" data-wow-delay="2.5s">
+                        <div class="image"><img src="img/products/brigde.jpg" alt=""></div>
+                        <div class="product-name">Brigde</div>
+                        <div class="readmore"><a href="javascript:void(0);">Explore Now</a></div>
+                    </li>
+                    <li class="wow fadeInLeft" data-wow-delay="3s">
+                        <div class="image"><img src="img/products/transportation.jpg" alt=""></div>
+                        <div class="product-name">Transportation</div>
+                        <div class="readmore"><a href="javascript:void(0);">Explore Now</a></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="project">
+        <div class="wrapper">
+            <div class="heading wow fadeInDown"><span>Welcome to</span>Projects</div>
+            <div class="project-tabs wow fadeInDown">
+                <ul>
+                    <li class="active">Completed Projects</li>
+                    <li>Under Compliation Projects</li>
+                </ul>
+            </div>
+            <div class="box">
+                <div class="project-list">
+                    <div class="left">
+                        <ul class="desktop-project-sec">
+                            <li class="wow fadeInUp" data-wow-delay="0.5s">
+                                <a href="javascript:void(0);">
+                                    <div class="image"><img src="img/bridge.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Bridge</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="wow fadeInUp" data-wow-delay="1s">
+                                <a href="javascript:void(0);">
+                                    <div class="image"><img src="img/airport.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Airport</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="wow fadeInUp" data-wow-delay="1.5s">
+                                <a href="javascript:void(0);">
+                                    <div class="image"><img src="img/road.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Road</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="wow fadeInUp" data-wow-delay="2s">
+                                <a href="javascript:void(0);">
+                                    <div class="image"><img src="img/highway.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Highway</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="wow fadeInUp" data-wow-delay="2.5s">
+                                <a href="javascript:void(0);">
+                                    <div class="image"><img src="img/industry.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Industry</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="wow fadeInUp" data-wow-delay="3s">
+                                <a href="javascript:void(0);">
+                                    <div class="image"><img src="img/tunnel.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Tunnel</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="service-list service-slider project-mobile-slider">
+                            <ul>
+                                <li class="wow fadeInLeft" data-wow-delay="1s">
+                                    <div class="image"><img src="img/road.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Bridge</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </li>
+                                <li class="wow fadeInLeft" data-wow-delay="1.5s">
+                                    <div class="image"><img src="img/road.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Highway</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </li>
+                                <li class="wow fadeInRight" data-wow-delay="2s">
+                                    <div class="image"><img src="img/road.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Industry</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </li>
+                                <li class="wow fadeInRight" data-wow-delay="2.5s">
+                                    <div class="image"><img src="img/road.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Tunnel</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </li>
+                                <li class="wow fadeInLeft" data-wow-delay="1s">
+                                    <div class="image"><img src="img/road.jpg" alt=""></div>
+                                    <div class="project-info">
+                                        <div class="project-name">Bridge</div>
+                                        <div class="place">Nagpur, Maharashtra</div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+        </div>
+    </section>
+    <section class="clients">
+        <div class="wrapper">
+            <div class="heading"><span>We work with top</span>clients</div>
+            <div class="clients-list">
+                <ul>
+                    <li class="wow fadeInUp" data-wow-delay="0.5s"><img src="img/clintele/clintele8.jpg" alt=""></li>
+                    <li class="wow fadeInUp" data-wow-delay="1s"><img src="img/clintele/clintele12.jpg" alt=""></li>
+                    <li class="wow fadeInUp" data-wow-delay="1.5s"><img src="img/clintele/clintele11.jpg" alt=""></li>
+                    <li class="wow fadeInUp" data-wow-delay="2s"><img src="img//clintele/clintele6.jpg" alt=""></li>
+                    <li class="wow fadeInUp" data-wow-delay="2.5s"><img src="img/clintele/clintele26.jpg" alt=""></li>
+                    <li class="wow fadeInUp" data-wow-delay="3s"><img src="img//clintele/clintele24.jpg" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+<?php include("footer.php"); ?>
